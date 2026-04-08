e7099e6bc2f604d57c26cb57a9deb70c9cad63a7
Sologaboo <gabrielmolinagabimolina2018@gmail.com>
2026-04-08 12:46:16 -0400

    Create java.js

---
+++ java.js
@@ -0,0 +1,23 @@
+    { 
+        title: "1X2",
+        info: "<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1(el equipo local gana), X (los equipos empatan), 2 (el equipo visitante gana).</p>",
+        detail: "<p>Con el mercado 1X2, solo debe de predecir el resultado final del partido. Tienes tres opciones: selecciona 1 si gana el local, X si el encuentro termina en empate, o 2 si crees que el visitante se llevará la victoria.</p>",
+        status: "<p>Fútbol</p>",
+        type: "fut-bet-rule-house"
+    },
+
+    {
+        title:"<p>Doble oportunidad</p>",
+        info: "<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles 1X (al final del partido el equipo local gana o empata), X2 (al final del partido el equipo visitante gana o empata), 12 (al final del partido gana equipo local o el equipo visitante).</p>",
+        detail: "<p>Con el mercado de doble oportunidad tiene tres opciones para ganar: 1X (gana local o empate), X2 (gana visitante o empate) o 12 (gana cualquiera de los dos). ¡Elige la que mejor se adapte a tu estrategia!</p>",
+        status: "<p>Fútbol</p>",
+        type: "fut-bet-rule-house",
+    },
+ 
+    {
+        title:"<p></p>",
+        info: "<p></p>",
+        detail: "<p></p>",
+        status: "<p></p>",
+        type: "",
+    },
