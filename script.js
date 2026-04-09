844c2c8d12e9cc19290e26eec31a02008a1e4b7c
Sologaboo <gabrielmolinagabimolina2018@gmail.com>
2026-04-09 07:10:41 -0400

    script.js

--- script.js
+++ script.js
@@ -5,59 +5,7 @@
  
 const wikiData = [
     { 
-        title: "Apuesta teaser",
-        info: "Una apuesta “teaser” es un tipo de apuesta combinada...",
-        detail: "Ejemplo práctico: En un teaser de 6 puntos en la NFL, si una línea está en -7, puedes moverla a -1 para facilitar la victoria.", // <--- NUEVA PROPIEDAD
-        status: "Estrategia",
-        type: "tip-bet-rule-house"
-    },
-     { 
-        title: "1X2",
-        info: "<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1(el equipo local gana), X (los equipos empatan), 2 (el equipo visitante gana).</p>",
-        detail: "<p>Con el mercado 1X2, solo debe de predecir el resultado final del partido. Tienes tres opciones: selecciona 1 si gana el local, X si el encuentro termina en empate, o 2 si crees que el visitante se llevará la victoria.</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house"
-    },
-
-    {
-        title:"<p>Doble oportunidad</p>",
-        info: "<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles 1X (al final del partido el equipo local gana o empata), X2 (al final del partido el equipo visitante gana o empata), 12 (al final del partido gana equipo local o el equipo visitante).</p>",
-        detail: "<p>Con el mercado de doble oportunidad tiene tres opciones para ganar: 1X (gana local o empate), X2 (gana visitante o empate) o 12 (gana cualquiera de los dos). ¡Elige la que mejor se adapte a tu estrategia!</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house",
-    },
- 
-    {
-        title:"<p>Total <b>(Más /Menos)</b></p>",
-        info: "<p>Se debe predecir si el número de goles marcados durante el partido estará por encima o por debajo de la línea estipulada.</p>",
-        detail: "<p>En el mercado de Total (Más/Menos), solo tiene que predecir cuántos goles se marcarán en total. Nosotros fijamos una cifra (línea) y usted elige si el resultado final será Más (por encima) o Menos (por debajo) de ese número. ¡No importa quién gane, solo los goles!</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house",
-    },
-		
-    {
-        title:"<p>¿Se clasifica?</p>",
-        info: "<p>Se debe predecir si el equipo seleccionado se clasificará para la siguiente fase del torneo.</p>",
-        detail: "<p>En la apuesta ¿Se clasifica?, no importa el marcador exacto, ni si el partido se define en tiempo extra o penales. Lo único que debe predecir es si el equipo que elijas logra avanzar a la siguiente ronda del torneo.</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house",
-    },
-		
-    {
-        title:"<p>Ambos equipos anotan <b>(GG/NG)</b></p>",
-        info: "<p>En este mercado hay dos posibles resultados: GG(ambos equipos marcan al menos un gol cada uno durante todo el partido), NG (uno o ambos no marcan ningún gol durante todo el partido).</p>",
-        detail: "<p>En el mercado GG/NG no importa quién gane el partido, solo si hay goles de ambos bandos. GG significa que ambos marcan; NG significa que alguno se queda sin anotar. ¡Así de simple es ganar!</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house",
-    },
-		
-    {
-        title:"<p>Apuesta sin empate <b>(DNB)</b></p>",
-        info: "<p></p>",
-        detail: "<p>Este mercado de apuestas consiste en lo siguiente, para definir una apuesta como ganadora, tiene que haber un equipo ganador. Lo que significa que, si el partido termina en empate, se reembolsará el dinero apostado. </p><p><b>Por ejemplo</b>,si un resultado final da como resultado un empate, la apuesta se liquidará como cancelada.</p>",
-        status: "<p>Fútbol</p>",
-        type: "fut-bet-rule-house",
-    },
+    }
 ];
 
 function searchWiki() {
